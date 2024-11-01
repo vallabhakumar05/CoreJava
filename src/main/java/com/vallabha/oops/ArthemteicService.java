efc94671f33d5121b8f0647d6ac52f2c319b6e1b
vallabhaneni413@gmail.com <vallabhakumar05@gmail.com>
2024-11-01 16:18:45 -0400

    git Hub checking with kumar

--- src/main/java/com/vallabha/oops/ArthemteicService.java
+++ src/main/java/com/vallabha/oops/ArthemteicService.java
@@ -4,7 +4,7 @@ public interface ArthemteicService {
     public int add(int a, int b);
     public int sub(int a, int b);
     public int mul(int a, int b);
-    public int div(int a, int b);
+    public int div(int a, int c);
     public int addMany(int... a);
     public int mulMany(int... d);
 
